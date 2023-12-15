54ea6b7124ffb90d30faaa9a01c94ec04f8380af
doantuan9x <tuanmdtk10.4@gmail.com>
2023-12-15 17:19:25 +0700

    create an reusable button

--- src/app/pages/demo/pages/shared/shared.module.ts
+++ src/app/pages/demo/pages/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
-
+import { ButtonsModule } from 'src/app/shared/buttons';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { SharedComponent } from './shared.component';
   ],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    SharedRoutingModule,
+    ButtonsModule
   ]
 })
 export class SharedModule { }
