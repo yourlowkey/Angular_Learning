a678d3918e408dd6075e7d1c82f787838e077179
doantuan9x <tuanmdtk10.4@gmail.com>
2023-12-15 17:52:26 +0700

    shared input module

---
+++ src/app/shared/controls/input/input.component.ts
@@ -0,0 +1,56 @@
+import { Component, EventEmitter, Input, OnInit, Output, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms'
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
+})
+export class InputComponent implements OnInit, ControlValueAccessor {
+  @Input() placeholder!: string;
+  @Output() changed = new EventEmitter<string>()
+
+  value!: string;
+  isDisable!: boolean
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  private propagateChange: any = () => {
+
+  }
+  private propagateTouched: any = () => {
+
+  }
+  writeValue(value: string): void {
+    this.value = value
+  }
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn
+  }
+  registerOnTouched(fn: any): void {
+    this.propagateTouched = fn
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisable = isDisabled
+  }
+  onKeyup(value: string): void {
+    this.value = value
+    this.propagateChange(value)
+    this.changed.emit(value)
+  }
+  onBlur(): void {
+    this.propagateTouched()
+  }
+
+}
