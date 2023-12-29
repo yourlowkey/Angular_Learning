13922dd00977c53c4fe9f633da6f28bcb1afa952
yourlowkey <m.phuc2552000@gmail.com>
2023-12-29 16:06:29 +0700

    init project, install packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { StoreModule } from '@ngrx/store';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
