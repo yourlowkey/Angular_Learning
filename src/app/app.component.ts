1f93a459490efee70e9de6147844eb5ec3b38aa6
yourlowkey <m.phuc2552000@gmail.com>
2023-12-29 16:06:29 +0700

    init project, install packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.log("env", environment.name);
+
+  }
   title = 'lpa-demo';
 }
