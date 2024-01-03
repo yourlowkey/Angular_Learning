ff3b39fcc870165b8c0ca581ef0af067526f8b33
yourlowkey <m.phuc2552000@gmail.com>
2024-01-03 09:41:11 +0700

    navgation edit

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+// import { DomSanitizer } from '@angular/platform-browser';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports: [MatIconModule, MatMenuModule, MatButtonModule]
 })
 export class HeaderComponent {
-
+  // constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+  //   // Note that we provide the icon here as a string literal here due to a limitation in
+  //   // Stackblitz. If you want to provide the icon from a URL, you can use:
+  //   // `iconRegistry.addSvgIcon('thumbs-up', sanitizer.bypassSecurityTrustResourceUrl('icon.svg'));`
+  //   iconRegistry.addSvgIconLiteral('thumbs-up', sanitizer.bypassSecurityTrustHtml(THUMBUP_ICON));
+  // }
 }
