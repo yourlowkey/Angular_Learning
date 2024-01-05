0e148e9a0bd89c80b07dd5684611edfaae46db94
yourlowkey <m.phuc2552000@gmail.com>
2024-01-05 16:33:54 +0700

    v1.0.7 assessment form datatable

--- src/app/pages/assessment-form/assessment-form.component.ts
+++ src/app/pages/assessment-form/assessment-form.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
-type Item = {
-  label: string,
-  value: number
-}
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ControlItem, Value } from 'src/app/models';
+import { AssessmentService } from 'src/app/services';
 @Component({
   selector: 'app-assessment-form',
   templateUrl: './assessment-form.component.html',
   styleUrls: ['./assessment-form.component.scss']
 })
 export class AssessmentFormComponent {
-  items!: Item[];
-  constructor() {
+  items!: ControlItem[];
+  value!: Value[];
+  kcbdmiExpressionPercent!: string;
+  kcbdmiGrammarPercent!: string;
+  @Output() changed = new EventEmitter<Value[]>()
+  constructor(private assessmentService: AssessmentService) {
     this.items = [
       {
         label: 'MacArthur - Bates Communication Development Assessment Record (K-MBCDI)',
@@ -23,5 +25,41 @@ export class AssessmentFormComponent {
       { label: 'Third', value: 3 },
       { label: 'Fourth', value: 4 },
       { label: 'Fifth', value: 5 }]
+  }
+
+  onChanged(value: Value, event: Event): void {
+    const element = event.currentTarget as HTMLInputElement
+    const checked = element.checked
+    console.log("checked on change", checked);
+
+    const selected = this.getSelected(value, checked)
+    this.value = selected
+    console.log("emit value checkbox", this.changed);
+
+    this.changed.emit(selected)
+  }
+  private getSelected(value: Value, checked: boolean): Value[] {
+    const selected: Value[] = this.value ? [...this.value] : [];
+    if (checked) {
+      if (!selected.includes(value)) {
+        selected.push(value)
+      }
+    }
+    else {
+      const index = selected.indexOf(value);
+      selected.splice(index, 1)
+      console.log("slice");
+    }
+    return selected
+  }
+  isChecked(value: Value): boolean {
+    console.log("checked", this.value && this.value.includes(value));
+    return this.value && this.value.includes(value)
+  }
+  onBlurKbcdmiExpresstionScore(value: number): void {
+    this.kcbdmiExpressionPercent = this.assessmentService.getKmbcdiExpressionPercent(value)
+  }
+  onBlurKbcdmiGrammarScore(value: number): void {
+    this.kcbdmiGrammarPercent = this.assessmentService.getKmbcdiGrammarPercent(value)
   }
 }
